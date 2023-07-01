b1e3c244859b17e8cec5ce3e838f4b939c344e93
shafou <jlmacedomatos@gmail.com>
2023-07-01 18:34:29 -0400

    fixed RustLastRun

--- lua/rust-tools/cached_commands.lua
+++ lua/rust-tools/cached_commands.lua
@@ -6,12 +6,13 @@ local cache = {
   last_runnable = nil,
 }
 
--- @param action 
-M.set_last_runnable = function(c, r)
+---@param choice integer
+---@param result table
+M.set_last_runnable = function(choice, result)
   cache.last_runnable = { choice, result }
 end
 
--- @param args 
+---@param args
 M.set_last_debuggable = function(args)
   cache.last_debuggable = args
 end
@@ -27,8 +28,9 @@ end
 
 M.execute_last_runnable = function()
   local action = cache.last_runnable
-  if action then 
-    rt.runnables.run_command(action[0], action[1])
+
+  if action then
+    rt.runnables.run_command(action[1], action[2])
   else
     rt.runnables.runnables()
   end
